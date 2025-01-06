02665f7311115cf7fc5797e124fc9439bffb14da
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-01-05 21:08:01 -0400

    Dto Medico

--- src/medicos/dto/create-medico.dto.ts
+++ src/medicos/dto/create-medico.dto.ts
@@ -1,1 +1,10 @@
-export class CreateMedicoDto {}
+export class CreateMedicoDto {
+  numeroColegiado: string;
+  especialidad: string;
+  fechaContratacion: Date;
+  activo: boolean;
+  fotoPerfil: string;
+  certificaciones: string[];
+  idiomas: string[];
+  horario_disponible: any;
+}
