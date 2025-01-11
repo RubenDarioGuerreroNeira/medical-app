0e86dee5188cdf39d3162a7c0d7f4d7c6b3706d7
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-01-11 11:40:14 -0400

    Receta Médica

--- src/receta-medica/receta-medica.module.ts
+++ src/receta-medica/receta-medica.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { RecetaMedicaService } from './receta-medica.service';
-import { RecetaMedicaController } from './receta-medica.controller';
+import { Module } from "@nestjs/common";
+import { RecetaMedica } from "../Entities/RecetaMedica";
+import { Cita } from "src/Entities/Cita.entity";
+import { RecetaMedicaService } from "./receta-medica.service";
+import { RecetaMedicaController } from "./receta-medica.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RecetaMedica, Cita])],
   controllers: [RecetaMedicaController],
   providers: [RecetaMedicaService],
 })
--- src/receta-medica/receta-medica.service.ts
+++ src/receta-medica/receta-medica.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@nestjs/common";
+
 import { CreateRecetaMedicaDto } from "./dto/create-receta-medica.dto";
 import { UpdateRecetaMedicaDto } from "./dto/update-receta-medica.dto";
 import { RecetaMedica } from "../Entities/RecetaMedica";
+import { Cita } from "src/Entities/Cita.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class RecetaMedicaService {
+  constructor(
+    @InjectRepository(RecetaMedica)
+    private recetaMedicaRepository: Repository<RecetaMedica>,
+    @InjectRepository(Cita)
+    private citaRepository: Repository<Cita>
+  ) {}
+
   create(createRecetaMedicaDto: CreateRecetaMedicaDto) {
     return "This action adds a new recetaMedica";
   }
