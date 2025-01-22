6b4222553b704e36ac9599e59ad04ab432b1b757
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-01-21 22:08:44 -0400

    citas module

--- src/citas/citas.module.ts
+++ src/citas/citas.module.ts
@@ -10,7 +10,6 @@ import { Medico } from "../Entities/Medico.entity";
 import { HistorialMedico } from "../Entities/HistorialMedico.entity";
 import { JwtStrategy } from "../auth/strategies/jwt.strategy";
 import { JwtAuthGuard } from "src/auth/Jwt-auth.guard";
-import { RolesGuard } from "../Guard/Guard";
 import { AuthModule } from "../auth/auth.module";
 
 @Module({
@@ -29,22 +28,7 @@ import { AuthModule } from "../auth/auth.module";
     }),
   ],
   controllers: [CitasController],
-  providers: [CitasService, JwtStrategy, RolesGuard, JwtAuthGuard],
+  providers: [CitasService, JwtStrategy, JwtAuthGuard],
   exports: [CitasService],
 })
 export class CitasModule {}
-
-// import { Module } from '@nestjs/common';
-// import { CitasService } from './citas.service';
-// import { CitasController } from './citas.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Cita } from '../Entities/Cita.entity';
-// import { Usuario } from '../Entities/Usuarios.entity';
-// import { Medico } from '../Entities/Medico.entity';
-// import { HistorialMedico } from '../Entities/HistorialMedico.entity';
-// @Module({
-//   imports: [TypeOrmModule.forFeature([Cita, Usuario, Medico, HistorialMedico])],
-//   controllers: [CitasController],
-//   providers: [CitasService],
-// })
-// export class CitasModule {}
