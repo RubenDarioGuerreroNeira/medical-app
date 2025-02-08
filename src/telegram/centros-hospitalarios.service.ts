0b401fef481fe2454b883960fd534ee2c602ee4e
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-02-07 23:57:14 -0400

    M{as Hospitales y Clinícas

--- src/telegram/centros-hospitalarios.service.ts
+++ src/telegram/centros-hospitalarios.service.ts
@@ -26,7 +26,7 @@ export class ClinicasVenezuelaService {
       ciudad: "San Cristóbal",
       direccion: "Av. Libertador - Rdoma dle Educador",
       telefono: "04140854174", // Agregado código de área
-      especialidades: ["Cardiología", "Pediatría", "Traumatología"],
+      especialidades: ["Todas las Especialidades"],
       horario: "24 horas",
       emergencia24h: true,
       coordenadas: {
@@ -41,7 +41,7 @@ export class ClinicasVenezuelaService {
       ciudad: "San Cristóbal",
       direccion: "La Guayana , Frente a la Escuela de Artes Plásticas",
       telefono: "04168963965", // Removidos espacios extra
-      especialidades: ["Medicina General", "Ginecología", "Oftalmología"],
+      especialidades: ["Todas las Especialidades"],
       horario: "Lunes a Domingo 24h",
       emergencia24h: true,
       coordenadas: {
@@ -57,7 +57,7 @@ export class ClinicasVenezuelaService {
       direccion:
         "Av. Principal de Pueblo Nuevo, Centro de Cirugía San Sebastián, Pueblo Nuevo, Al lado de Residencias el Bosque",
       telefono: "04149635474", // Removido espacio al final
-      especialidades: ["Medicina General", "Ginecología", "Oftalmología"],
+      especialidades: ["Todas las Especialidades"],
       horario: "Lunes a Domingo 24h",
       emergencia24h: true,
       coordenadas: {
@@ -72,7 +72,7 @@ export class ClinicasVenezuelaService {
       ciudad: "San Cristóbal",
       direccion: "Centro Comercial Paseo la Villa, Av. Guayana",
       telefono: "04160897020", // Corregido número
-      especialidades: ["Medicina General", "Ginecología", "Oftalmología"],
+      especialidades: ["Todas las Especialidades"],
       horario: "Lunes a Domingo 24h",
       emergencia24h: true,
       coordenadas: {
@@ -80,6 +80,92 @@ export class ClinicasVenezuelaService {
         lng: -72.225904,
       },
     },
+
+    {
+      id: "5",
+      nombre: "Protección Civil",
+      estado: "Táchira",
+      ciudad: "San Cristóbal",
+      direccion: "Pueblo Nuevo Parte Alta, Cruce a mano Izquierda de los PAbellones",
+      telefono: "04160897020", // Corregido número
+      especialidades: ["Primeros Auxilios"],
+      horario: "Lunes a Domingo 24h",
+      emergencia24h: true,
+      coordenadas: {
+        lat: 7.787633,
+        lng: -72.202968,
+      },
+    },
+
+
+    {
+      id: "6",
+      nombre: "Policliníca Táchira",
+      estado: "Táchira",
+      ciudad: "San Cristóbal",
+      direccion: "Av 19 de Abril, frente a Residencias el Parque, Parque Metropolitano",
+      telefono: "04160897020", // Corregido número
+      especialidades: ["Todas las Especialidades"],
+      horario: "Lunes a Domingo 24h",
+      emergencia24h: true,
+      coordenadas: {
+        lat: 7.757779,
+        lng: -72.218912,
+      },   
+    },
+
+
+    {
+      id: "7",
+      nombre: "Hospital Central",
+      estado: "Táchira",
+      ciudad: "San Cristóbal",
+      direccion: "Av Lucio Oquendo Frente al Colegio de Médicos",
+      telefono: "04160897020", 
+      especialidades: ["Todas las Especialidades"],
+      horario: "Lunes a Domingo 24h",
+      emergencia24h: true,
+      coordenadas: {
+        lat: 7.756495,     
+        lng: -72.226321 
+      },
+    },
+
+    {
+      id: "8",
+      nombre: "Centro Médico Club de Leones",
+      estado: "Táchira",
+      ciudad: "San Cristóbal",
+      direccion: "Av. Principal La Castra, San Cristóbal 5001, Táchira",
+      telefono: "04160897020", 
+      especialidades: ["Todas las Especialidades"],
+      horario: "Lunes a Domingo 24h",
+      emergencia24h: true,
+      coordenadas: {
+        lat: 7.752030,
+        lng: -72.226182 
+      },
+    },
+
+
+ {
+      id: "9",
+      nombre: "Hospital Militar",
+      estado: "Táchira",
+      ciudad: "San Cristóbal",
+      direccion: "Via la Cueva del Oso Pueblo Nuevo ",
+      telefono: "04160897020", 
+      especialidades: ["Todas las Especialidades"],
+      horario: "Lunes a Domingo 24h",
+      emergencia24h: true,
+      coordenadas: {
+        lat: 7.798255, 
+        lng: -72.194435
+      },
+    },
+ 
+
+
   ];
 
   async buscarClinicas(params: {
