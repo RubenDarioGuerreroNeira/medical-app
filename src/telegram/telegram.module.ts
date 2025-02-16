98e626cb798027550fe56e65dc66b3f7a865f420
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-02-16 00:48:24 -0400

    limpio el Telegram Module e integracion de sonido l recordatorio

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -19,7 +19,6 @@ import { MedicationReminder } from "src/entities/reminder.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TelegramNotificationService } from "./telegramNotificationService.service";
 import { ReminderService as ServiceReminder } from "./reminder.service";
-// import TelegramBot from "node-telegram-bot-api";
 import * as TelegramBot from "node-telegram-bot-api";
 
 @Module({
@@ -68,54 +67,3 @@ import * as TelegramBot from "node-telegram-bot-api";
 })
 export class TelegramModule {}
 
-// // telegram.module.ts
-// import { Module } from "@nestjs/common";
-// import { TelegramService } from "./telegram.service";
-// import { TelegramController } from "./telegram.controller";
-// import { ConfigModule } from "@nestjs/config";
-// import { GeminiAIService } from "src/Gemini/gemini.service";
-// import { ClinicasVenezuelaService } from "./centros-hospitalarios.service";
-// import { TelegramLocationHandler } from "./telegram-location-handler.service";
-// import { TelegramMessageFormatter } from "./telegramMessageFormatter.service";
-// import { OSMService } from "./farmacias-maps.service";
-// import { TelegramErrorHandler } from "./telegramErrorHandler.service";
-// import { TelegramWebhookController } from "./telegramWebhook.controller";
-// import { TelegramBotService } from "./telegramBotService.service";
-// import { TelegramDiagnosticService } from "./telegramDiagnosticService.service";
-// import { ReminderService } from "./reminder.service";
-// import { ReminderCommands } from "./reminder.commnads";
-// import { ScheduleModule } from "@nestjs/schedule";
-// import { SchedulerRegistry } from "@nestjs/schedule";
-// import { MedicationReminder } from "src/entities/reminder.entity";
-// import { TypeOrmModule } from "@nestjs/typeorm";
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot(),
-//     TypeOrmModule.forFeature([MedicationReminder]),
-//     ScheduleModule.forRoot(),
-//   ],
-//   providers: [
-//     GeminiAIService,
-//     ClinicasVenezuelaService,
-//     TelegramLocationHandler,
-//     TelegramMessageFormatter,
-//     OSMService,
-//     TelegramErrorHandler,
-//     TelegramBotService,
-//     TelegramDiagnosticService,
-//     ReminderCommands,
-//     SchedulerRegistry, // Asegúrate de que SchedulerRegistry esté incluido
-//     {
-//       provide: ReminderService,
-//       useClass: ReminderService,
-//     },
-//     {
-//       provide: TelegramService,
-//       useClass: TelegramService,
-//     },
-//   ],
-//   controllers: [TelegramController, TelegramWebhookController],
-//   exports: [TelegramService, TelegramBotService],
-// })
-// export class TelegramModule {}
