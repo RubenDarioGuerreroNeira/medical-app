79932bf9b856a43ee4d4b78e9bd0c8ce3e643d74
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-04-02 17:32:14 -0400

    Agregar construcción de TypeScript en el flujo de trabajo de CI y actualizar la configuración de entidades en data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,37 +1,40 @@
-import { Usuario } from "./entities/usuarios.entity";
-import { Medico } from "./entities/medico.entity";
-import { Cita } from "./entities/cita.entity";
-import { HistorialMedico } from "./entities/historialmedico.entity";
-import { RecetaMedica } from "./entities/recetamedica";
-import { DocumentoConsulta } from "./entities/documentoconsulta";
-import { NotaMedica } from "./entities/notamedica";
-import { MedicationReminder } from "./entities/reminder.entity";
+import { Usuario } from './entities/usuarios.entity';
+import { Medico } from './entities/medico.entity';
+import { Cita } from './entities/cita.entity';
+import { HistorialMedico } from './entities/historialmedico.entity';
+import { RecetaMedica } from './entities/recetamedica';
+import { DocumentoConsulta } from './entities/documentoconsulta';
+import { NotaMedica } from './entities/notamedica';
+import { MedicationReminder } from './entities/reminder.entity';
 
-import { DataSource } from "typeorm";
-import { join } from "path";
-import * as dotenv from "dotenv";
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   // entities: ["src/**/*.entity{.ts,.js}"],
-  entities: [
-    Usuario,
-    Medico,
-    Cita,
-    HistorialMedico,
-    RecetaMedica,
-    DocumentoConsulta,
-    NotaMedica,
-    MedicationReminder,
-  ],
-  migrations: [join(__dirname, "migrations", "*.{ts,js}")],
+  // entities: [
+  //   Usuario,
+  //   Medico,
+  //   Cita,
+  //   HistorialMedico,
+  //   RecetaMedica,
+  //   DocumentoConsulta,
+  //   NotaMedica,
+  //   MedicationReminder,
+  // ],
+
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+
+  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
   synchronize: false,
   logging: true,
 });
