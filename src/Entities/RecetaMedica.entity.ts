7ca3c3de7f42bb9ca91a08f1feb4a143492d6fd1
RubenDarioGuerreroNeira <rudargeneira@gmail.com>
2025-04-03 00:25:46 -0400

    Agregar archivos de entidad con nombres corregidos

---
+++ src/Entities/RecetaMedica.entity.ts
@@ -0,0 +1,30 @@
+import { Cita } from './Cita.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('receta_medica')
+export class RecetaMedica {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Cita)
+  @JoinColumn({ name: 'cita_id' })
+  cita: Cita;
+
+  @Column({ type: 'text' })
+  medicamentos: string;
+
+  @Column({ type: 'text' })
+  indicaciones: string;
+
+  @Column({ type: 'timestamp' })
+  fecha_emision: Date;
+
+  @Column({ nullable: true })
+  archivo_url: string;
+}
